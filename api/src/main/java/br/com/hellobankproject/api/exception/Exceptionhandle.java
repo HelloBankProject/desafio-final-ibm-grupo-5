33cce9c628b1dabeea0352032ae745282ce0779d
Andre <andre_oliiveira97@hotmail.com>
2022-09-11 20:43:29 -0300

    fix validation

--- api/src/main/java/br/com/hellobankproject/api/exception/Exceptionhandle.java
+++ api/src/main/java/br/com/hellobankproject/api/exception/Exceptionhandle.java
@@ -10,14 +10,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @ControllerAdvice
 public class Exceptionhandle {
 
     @ResponseBody
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(WebExchangeBindException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ValidationExceptions error(MethodArgumentNotValidException e) {
+    public ValidationExceptions error(WebExchangeBindException e) {
 
         Map<String, String> erros = new HashMap<>();
         e.getBindingResult().getFieldErrors().forEach(error -> erros.put(error.getField(), error.getDefaultMessage()));
